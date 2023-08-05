97be0a2eaa5400afefd28f2a66710f7b4067bda4
chichoon <chichoon.choi@gmail.com>
2023-08-05 23:04:13 +0900

    feat: :sparkles: App 의 라우터에 TodoPage 등록

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import { SigninPage, SignupPage } from "pages";
 import { Layout } from "components";
 import { useCheckLogin } from "hooks";
+import { TodoPage } from "pages/TodoPage";
 
 function App() {
   useCheckLogin();
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/todo" element={<div>todo</div>} />
+        <Route path="/todo" element={<TodoPage />} />
       </Route>
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/signin" element={<SigninPage />} />
