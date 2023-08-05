35f974e8babe74c319ac8a706ef2e5bdde17ed24
chichoon <chichoon.choi@gmail.com>
2023-08-05 23:34:36 +0900

    feat: :sparkles: Todo Element 추가

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 
-import { AddTodoForm } from "components";
+import { AddTodoForm, TodoElement } from "components";
 import { getTodos } from "services/todoInstance";
 import { Todo } from "types/todo";
 
 export const TodoPage = () => {
   const [todoList, setTodoList] = useState<Todo[]>([]);
+
   useEffect(() => {
     getTodos().then((res) => {
       setTodoList(res);
@@ -17,7 +18,7 @@ export const TodoPage = () => {
       <AddTodoForm setTodoList={setTodoList} />
       <ul>
         {todoList.map((todo) => (
-          <li key={todo.id}>{todo.todo}</li>
+          <TodoElement key={todo.id} todo={todo} setTodoList={setTodoList} />
         ))}
       </ul>
     </>
